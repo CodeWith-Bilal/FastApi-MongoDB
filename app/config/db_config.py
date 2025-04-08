896ed91ffe2b55c44d5ad6541b04a6334efd089a
CodeWithMian <mb03086350364@gmail.com>
2025-04-08 10:33:21 +0500

    Move all the dependancy to uv.lock

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -5,6 +5,13 @@ import os
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 DB_NAME = "fastapi_auth"
 
+
+try:
+    client = AsyncIOMotorClient(MONGO_URI)
+    client.admin.command('ping')
+except Exception as e:
+    print("MongoDB connection error:", e)
+    raise
+
 client = AsyncIOMotorClient(MONGO_URI)
 database: Database = client[DB_NAME]
-
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.routes import user_route
-
+from app.routes import auth_route
+from dotenv import load_dotenv
 app = FastAPI()
+load_dotenv()
 
-app.include_router(user_route.router, prefix="/api/users", tags=["Users"])
+app.include_router(auth_route.router, prefix="/api", tags=["Users"])
 
